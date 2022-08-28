10b8390a3841d8cc6dfc351ab2a50d44ecfe2afa
Radomir Epur <radomir9720@gmail.com>
2022-08-28 18:16:28 +0300

    chore: upgrade packages
    * fix: linter hints

--- lib/src/extensions/list_extensions.dart
+++ lib/src/extensions/list_extensions.dart
@@ -1,5 +1,6 @@
 extension ListExtension<T> on List<T> {
   Iterable<T> divideBy(T divider) sync* {
+    // ignore: no_leading_underscores_for_local_identifiers
     final _iterator = iterator;
     final isNotEmpty = _iterator.moveNext();
     if (!isNotEmpty) return;
