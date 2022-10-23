23fd829e65840ef14ade1c5cb7800a087aa484f9
Radomir Epur <radomir9720@gmail.com>
2022-10-23 17:33:20 +0300

    feat: added completer to debouncer

--- lib/src/utils/debouncer.dart
+++ lib/src/utils/debouncer.dart
@@ -5,16 +5,29 @@ class Debouncer {
 
   Timer? _timer;
 
+  Completer<void>? _completer;
+
+  Future<void> get completerFuture => _completer?.future ?? Future.value();
+
+  bool get isCompleted => _completer?.isCompleted ?? true;
+
   Debouncer({required this.milliseconds});
 
   void run(void Function() action) {
+    if (isCompleted) _completer = Completer();
     if (_timer != null) {
       _timer!.cancel();
     }
-    _timer = Timer(Duration(milliseconds: milliseconds), action);
+    _timer = Timer(Duration(milliseconds: milliseconds), () {
+      action();
+      if (!isCompleted) _completer?.complete();
+    });
   }
 
   void dispose() {
     _timer?.cancel();
+    if (!isCompleted) {
+      _completer?.complete();
+    }
   }
 }
