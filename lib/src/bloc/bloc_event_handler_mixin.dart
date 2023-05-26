f30854812d0d1384349397e274aa0862b3ce752e
Radomir Epur <radomir9720@gmail.com>
2023-05-26 20:43:00 +0300

    feat: added BlocEventHandlerMixin.handleEvent

--- lib/src/bloc/bloc_event_handler_mixin.dart
+++ lib/src/bloc/bloc_event_handler_mixin.dart
@@ -5,6 +5,29 @@ typedef StateBuilder<State> = State Function();
 
 mixin BlocEventHandlerMixin<Event extends EffectEvent, State>
     on Bloc<Event, State> {
+  void handleEvent<E extends Event, ActionResult>({
+    required StateBuilder<State> inLoading,
+    required StateBuilder<State> inFailure,
+    required Future<ActionResult> Function() action,
+    required Future<State> Function(ActionResult actionResult) onActionResult,
+  }) {
+    return on<E>(
+      (event, emit) async {
+        emit(inLoading());
+
+        try {
+          final res = await action();
+
+          emit(await onActionResult(res));
+        } on Object catch (_) {
+          emit(inFailure());
+
+          rethrow;
+        }
+      },
+    );
+  }
+
   Future<void> handle<ActionResult>({
     required Event event,
     required Emitter<State> emit,
